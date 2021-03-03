2e97dea83f82cd60693db8a33461cb4e21ac2abd
bangtidy <bangtidy@qq.com>
2021-03-03 09:44:37 +0800

    环境搭建

---
+++ guli_teacher/src/main/java/com/guli/teacher/mapper/EduTeacherMapper.java
@@ -0,0 +1,16 @@
+package com.guli.teacher.mapper;
+
+import com.guli.teacher.entity.EduTeacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author bangtidy
+ * @since 2021-03-02
+ */
+public interface EduTeacherMapper extends BaseMapper<EduTeacher> {
+
+}
---
+++ guli_teacher/src/main/java/com/guli/teacher/service/EduTeacherService.java
@@ -0,0 +1,16 @@
+package com.guli.teacher.service;
+
+import com.guli.teacher.entity.EduTeacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author bangtidy
+ * @since 2021-03-02
+ */
+public interface EduTeacherService extends IService<EduTeacher> {
+
+}
